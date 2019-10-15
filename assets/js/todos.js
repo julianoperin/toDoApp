d4066a34bb93c538f89689db5e78d88bef3ff55c
Juliano Perin <julianoperins@gmail.com>
2019-10-14 23:46:32 -0400

    Toggled + sign

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -21,6 +21,10 @@ $("input[type='text']").keypress(function(event){
     var todoText = $(this).val();
     $(this).val("");
     //create a new li and add to ul
-    $("ul").append("<li><span>X</span> " + todoText + "</li>")
+    $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>")
     }
+});
+
+$(".fa-plus-circle").click(function(){
+    $("input[type='text']").fadeToggle();
 });
